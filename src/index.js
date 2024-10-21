3fd19207dde3a0d0649fb08c9645c40e5eed3375
Pershiba-shibatech <shibatech06@gmail.com>
2024-10-21 22:40:13 +0530

    url

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import express from 'express'
 import mongoose from 'mongoose'
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import cors from 'cors'
 
 // import { v2 as cloudinary } from 'cloudinary'
 import { Routes } from './User/routes.js'
 
-// dotenv.config()
+dotenv.config()
 const PORT = "4000"
-const MONGOURI = "mongodb+srv://shibatech06:WGusDjuuIdUiIYvi@educationalportal.dqe7jah.mongodb.net/educationalportal?retryWrites=true&w=majority"
+const MONGOURI = "mongodb+srv://shibatech06:WGusDjuuIdUiIYvi@educationalportal.dqe7jah.mongodb.net/educationalportal"
 // const CLOUDINARY_CLOUD_NAME = "dwm8g1j8i"
 // const CLOUDINARY_API_KEY = "745971731583546"
 // const CLOUDINARY_API_SECRET = "rs96Q4RdDvys-NUwubZHvoGShj8"
 // const CLOUDINARY_URL = "cloudinary://745971731583546:rs96Q4RdDvys-NUwubZHvoGShj8@dwm8g1j8i"
-const mongoString = MONGOURI;
+const mongoString = process.env.MONGOURI;
 
 // const cloudinary = require('cloudinary').v2;
 
@@ -28,17 +28,19 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    mongoose.connect(mongoString);
+    mongoose.connect(mongoString, {
+        serverSelectionTimeoutMS: 30000 // Adjust this value as needed
+    });
     const database = mongoose.connection
     database.once('connected', () => {
         console.log('Database Connected');
     })
-  
+
     // cloudinary.config({
     //     cloud_name: CLOUDINARY_CLOUD_NAME,
     //     api_key: CLOUDINARY_API_KEY,
     //     api_secret: CLOUDINARY_API_SECRET
-        
+
     // });
 
     database.on('error', (error) => {
